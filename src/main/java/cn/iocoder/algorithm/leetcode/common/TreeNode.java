ddd96d07a4360d8d42621f57a97fc68d35512f81
YunaiV <>
2019-08-03 09:24:23 +0800

    leetcode 101、111、266、337、437、556、572、617、666、687

--- src/main/java/cn/iocoder/algorithm/leetcode/common/TreeNode.java
+++ src/main/java/cn/iocoder/algorithm/leetcode/common/TreeNode.java
@@ -1,5 +1,8 @@
 package cn.iocoder.algorithm.leetcode.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreeNode {
 
     public int val;
@@ -7,15 +10,32 @@ public class TreeNode {
     public TreeNode right;
     public TreeNode(int x) { val = x; }
 
-    public static TreeNode create(int... values) {
+    public static TreeNode create(Integer... values) {
         if (values.length == 0) {
             return null;
         }
 
-        TreeNode root = new TreeNode(values[0]);
+        List<TreeNode> nodes = new ArrayList<>(values.length + 1);
+        nodes.add(null);
+        for (int i = 1; i <= values.length; i++) {
+            Integer value = values[i - 1];
+            if (value == null) {
+                continue;
+            }
+            TreeNode node = new TreeNode(value);
+            nodes.add(node);
+            // 非根节点，添加到父节点下
+            if (i > 1) {
+                TreeNode parent = nodes.get(i / 2);
+                if (i % 2 == 0) {
+                    parent.left = node;
+                } else {
+                    parent.right = node;
+                }
+            }
+        }
 
-
-        return root;
+        return nodes.get(1);
     }
 
 }
