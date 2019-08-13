51fdcd5668f56906a7f1cbba720d137b9d8b9e99
YunaiV <>
2019-08-13 22:26:03 +0800

    leetcode 169、229

---
+++ src/main/java/cn/iocoder/algorithm/leetcode/no0229/Solution02.java
@@ -0,0 +1,11 @@
+package cn.iocoder.algorithm.leetcode.no0229;
+
+/**
+ * https://leetcode-cn.com/problems/majority-element-ii/
+ *
+ * Boyer-Moore 投票算法的升级版
+ *
+ * 超过 n/3 个元素的，个数可以是 0 个、1 个、2 个。
+ */
+public class Solution02 {
+}
