fd4342dd771f370dbe4332f8588d99e043c3912d
YunaiV <>
2019-08-17 11:28:06 +0800

    leetcode 433

--- src/main/java/cn/iocoder/algorithm/leetcode/no0126/Solution02.java
+++ src/main/java/cn/iocoder/algorithm/leetcode/no0126/Solution02.java
@@ -47,10 +47,10 @@ public class Solution02 {
         List<Node> results = new ArrayList<>();
         Queue<Node> queue = new LinkedList<>();
         queue.add(new Node(beginWord));
-        Set<String> visited = new HashSet<>();
+        Set<String> visited = new HashSet<>(); // 截止至上一轮，已经访问过的节点
         visited.add(beginWord);
         while (!queue.isEmpty()) {
-            Map<String, Node> currentNodes = new HashMap<>();
+            Map<String, Node> currentNodes = new HashMap<>(); // 当前轮，新产生的访问节点
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 Node node = queue.poll();
@@ -60,33 +60,38 @@ public class Solution02 {
                         continue;
                     }
                     for (String target : array) {
+                        // 已经找到，添加到 results 结果
                         if (target.equals(endWord)) {
                             results.add(new Node(target, node));
                             continue;
                         }
+                        // 上一轮已经访问过，跳过
                         if (visited.contains(target)) {
                             continue;
                         }
+                        // 本轮刚访问过，添加到已访问的节点上
                         Node currentVisitNode = currentNodes.get(target);
                         if (currentVisitNode != null) {
                             currentVisitNode.add(node);
                             continue;
                         }
+                        // 本轮未访问过，创建该新节点
                         currentVisitNode = new Node(target, node);
                         currentNodes.put(target, currentVisitNode);
                         queue.add(currentVisitNode);
                     }
                 }
             }
+            // 已经找到，结束
             if (!results.isEmpty()) {
                 break;
             }
+            // 将本轮访问过的，添加到 visited 中
             visited.addAll(currentNodes.keySet());
         }
 
         // 转换返回
-        List<List<String>> result = this.genResult(results);
-        return result;
+        return  this.genResult(results);
     }
 
     private List<List<String>> genResult(List<Node> nodes) {
