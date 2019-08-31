25aa446d84d38a66032b4c0f8ed2df3060771f00
YunaiV <>
2019-08-31 20:37:21 +0800

    leetcode 301

--- src/main/java/cn/iocoder/algorithm/leetcode/no0301/Solution.java
+++ src/main/java/cn/iocoder/algorithm/leetcode/no0301/Solution.java
@@ -1,38 +1,39 @@
 package cn.iocoder.algorithm.leetcode.no0301;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * https://leetcode-cn.com/problems/remove-invalid-parentheses/
  *
- * BFS
+ * BFS + 逐步拼凑合理的组合。
+ *
+ * 未通过，还在找原因。错误案例是 ")()))())))" ，答案是 ["(())","()()"] ，目前输出是 ["()()"]
+ *
+ * 简化案例为 ())()) 。因为第 108 行代码，固话了答案为 (()) ，无法获得 ()() 答案。
  */
 public class Solution {
 
     public class Node {
 
+        private String s;
         private String text;
         private int lastIndex;
 
-        public Node(String text, int lastIndex) {
+        public Node(String s, String text, int lastIndex) {
+            this.s = s;
             this.text = text;
             this.lastIndex = lastIndex;
         }
     }
 
-    private List<String> result;
+    private Set<String> result;
     private Queue<Node> queue;
-    private String s;
 
     public List<String> removeInvalidParentheses(String s) {
-        this.result = new ArrayList<>();
+        this.result = new HashSet<>();
         this.queue = new LinkedList<>();
-        this.s = s;
         // 初始队列
-        queue.add(new Node("", 0));
+        queue.add(new Node(s,"", 0));
 
         // bfs 开始
         while (!queue.isEmpty()) {
@@ -46,25 +47,19 @@ public class Solution {
                 break;
             }
         }
-//        if (result.isEmpty()) {
-//            result.add(this.removeLeftAndRight());
-//        }
-        return result;
+        return new ArrayList<>(result);
     }
 
     private void processNode(Node node) {
         // 寻找 ) 出现更多的首个位置
+        String s = node.s;
         int leftCount = 0;
         int rightCount = 0;
         int rightMoreIndex = -1; // 首次出现 ) 更多的位置
-        int leftMoreFirstIndex = -1; // 首次出现 ( 比 ) 多的位置
         for (int i = node.lastIndex; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (ch == '(') {
                 leftCount++;
-                if (leftMoreFirstIndex == -1) {
-                    leftMoreFirstIndex = i;
-                }
                 continue;
             }
             if (ch == ')') {
@@ -72,8 +67,6 @@ public class Solution {
                 if (rightCount > leftCount) {
                     rightMoreIndex = i;
                     break;
-                } else if (rightCount == leftCount) {
-                    leftMoreFirstIndex = -1;
                 }
             }
         }
@@ -85,38 +78,19 @@ public class Solution {
                 result.add(node.text + s.substring(node.lastIndex));
                 return;
             }
-            // 处理因为删除第一个 ( 括号，导致配对不对的问题。例如说 "(((k()(("
-            int nextRightIndex = -1;
-            for (int i = leftMoreFirstIndex + 1; i < s.length(); i++) {
-                if (s.charAt(i) == ')') {
-                    nextRightIndex = i;
-                    break;
+            // 尝试删除一个 ( 括号
+            for (int i = node.lastIndex; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                if (ch != '(') {
+                    continue;
                 }
-            }
-            // 找不到下一个 ) 括号，说明无法持平，所以删除从 node.lastIndex 开始的所有 ( 括号
-            if (nextRightIndex == -1) {
-                result.add(node.text + s.substring(node.lastIndex).replaceAll("\\(", ""));
-                return;
-            }
-            // 尝试删除第一个 ( 括号
-            if (leftMoreFirstIndex != -1) {
-                for (int i = leftMoreFirstIndex; i < nextRightIndex; i++) {
-                    char ch = s.charAt(i);
-                    if (ch != '(') {
-                        continue;
-                    }
-                    // 和前一个一样，删除意味着相同。
-                    if (i > leftMoreFirstIndex && ch == s.charAt(i - 1)) {
-                        continue;
-                    }
-                    queue.add(new Node(node.text
-                            + s.substring(node.lastIndex, leftMoreFirstIndex)
-                            + s.substring(leftMoreFirstIndex, i).replaceAll("\\(", "")
-                            + "("
-                            + s.substring(i + 1, nextRightIndex + 1).replaceAll("\\(", ""),
-                            nextRightIndex + 1));
+                // 和前一个一样，删除意味着相同。
+                if (i > node.lastIndex && ch == s.charAt(i - 1)) {
+                    continue;
                 }
-//                queue.add(new Node(node.text + s.substring(node.lastIndex, leftMoreFirstIndex), leftMoreFirstIndex + 1));
+                queue.add(new Node(s.substring(0, i) + s.substring(i + 1),
+                        node.text,
+                        node.lastIndex));
             }
             return;
         }
@@ -131,24 +105,10 @@ public class Solution {
             if (i > node.lastIndex && ch == s.charAt(i - 1)) {
                 continue;
             }
-            queue.add(new Node(node.text
-                    + s.substring(node.lastIndex, i)
-                    + s.substring(i + 1, rightMoreIndex + 1),
+            queue.add(new Node(s,node.text + s.substring(node.lastIndex, i) + s.substring(i + 1, rightMoreIndex + 1),
                     rightMoreIndex + 1));
         }
     }
-
-//    private String removeLeftAndRight() {
-//        String str = "";
-//        for (int i = 0; i < s.length(); i++) {
-//            char ch = s.charAt(i);
-//            if (ch == '(' || ch == ')') {
-//                continue;
-//            }
-//            str += ch;
-//        }
-//        return str;
-//    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -161,8 +121,13 @@ public class Solution {
 //        System.out.println(solution.removeInvalidParentheses(")()("));
 //        System.out.println(solution.removeInvalidParentheses(")()(x"));
 //        System.out.println(solution.removeInvalidParentheses(")()(("));
-        System.out.println(solution.removeInvalidParentheses(")()(()"));
+//        System.out.println(solution.removeInvalidParentheses(")()(()"));
 //        System.out.println(solution.removeInvalidParentheses("(((k()(("));
+//        System.out.println(solution.removeInvalidParentheses("(k))"));
+//        System.out.println(solution.removeInvalidParentheses("(k()"));
+//        System.out.println(solution.removeInvalidParentheses("(k(())"));
+//        System.out.println(solution.removeInvalidParentheses(")()("));
+        System.out.println(solution.removeInvalidParentheses("())())"));
     }
 
 }
